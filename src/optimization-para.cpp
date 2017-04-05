051be7817bee20555ca86dec399e7f0643436391
Mario CASANOVA <E061109Y@S046V7pc08.ensinfo.sciences.univ-nantes.prive>
2017-04-05 16:52:18 +0200

    changements optimization-para

--- src/optimization-para.cpp
+++ src/optimization-para.cpp
@@ -2,24 +2,26 @@
   Branch and bound algorithm to find the minimum of continuous binary 
   functions using interval arithmetic.
 
-  Sequential version
+  Mpi + OMP version
 
-  Author: Frederic Goualard <Frederic.Goualard@univ-nantes.fr>
-  v. 1.0, 2013-02-15
+  Author: Araya Montalvo , Casanova Mario
+
+  Warning : -Ne marche que pour un nombre de machine égale à une puissance de 2 (1,2,4,8,etc...)
+
+  v. 1.0, 2016-04-05
+
 */
 
 #include <iostream>
 #include <iterator>
 #include <string>
 #include <stdexcept>
-#include <vector>
-#include <queue>
+#include <iomanip>
 #include "interval.h"
 #include "functions.h"
 #include "minimizer.h"
-#include <mpi.h>
-//#include "C:\cygwin\usr\include\mpi.h"
-
+#include "mpi.h"
+#include "omp.h"
 using namespace std;
 
 
@@ -45,7 +47,6 @@ void minimize(itvfun f,  // Function to minimize
 	      minimizer_list& ml) // List of current minimizers
 {
   interval fxy = f(x,y);
-  
   if (fxy.left() > min_ub) { // Current box cannot contain minimum?
     return ;
   }
@@ -66,28 +67,47 @@ void minimize(itvfun f,  // Function to minimize
     ml.insert(minimizer{x,y,fxy.left(),fxy.right()});
     return ;
   }
-
   // The box is still large enough => we split it into 4 sub-boxes
   // and recursively explore them
   interval xl, xr, yl, yr;
   split_box(x,y,xl,xr,yl,yr);
+  /*#pragma omp parallel
+  #pragma omp single
+  { 
+    #pragma omp task
+    minimize(f,xl,yl,threshold,min_ub,ml);
+    #pragma omp task
+    minimize(f,xl,yr,threshold,min_ub,ml);
+    #pragma omp task
+    minimize(f,xr,yl,threshold,min_ub,ml);
+    #pragma omp task
+    minimize(f,xr,yr,threshold,min_ub,ml);
 
-  minimize(f,xl,yl,threshold,min_ub,ml);
-  minimize(f,xl,yr,threshold,min_ub,ml);
-  minimize(f,xr,yl,threshold,min_ub,ml);
-  minimize(f,xr,yr,threshold,min_ub,ml);
+  }  */
+    minimize(f,xl,yl,threshold,min_ub,ml);
+    minimize(f,xl,yr,threshold,min_ub,ml);
+    minimize(f,xr,yl,threshold,min_ub,ml);
+    minimize(f,xr,yr,threshold,min_ub,ml);
+
+
 }
 
 
 int main(int argc, char * argv[])
 {
- /* cout.precision(16);
+  int rank, numProcs;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  cout.precision(16);
   // By default, the currently known upper bound for the minimizer is +oo
   double min_ub = numeric_limits<double>::infinity();
+
   // List of potential minimizers. They may be removed from the list
   // if we later discover that their smallest minimum possible is 
   // greater than the new current upper bound
-  minimizer_list minimums;
   // Threshold at which we should stop splitting a box
   double precision;
 
@@ -98,94 +118,154 @@ int main(int argc, char * argv[])
   opt_fun_t fun;
   
   bool good_choice;
-  
-  // MPI vars
-  int numprocs, rank, size;
-  interval localx, localy;
-  
+
+  //Variable MPI
+  //interval myslice[1];
+  double minUpAll=42.0;
+  interval tabInterval[numProcs];
+  interval tabIntervalY[numProcs];
   queue<interval> subBoxes;
-  bool enough_boxes;
-  
-  // MPI initialization
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+  int SZ;
+  int localSZ;
   // Asking the user for the name of the function to optimize
-  if(rank == 0) {
-		do {
-		  good_choice = true;
+  if(rank == 0){
+    do {
+      good_choice = true;
 
-		  cout << "Which function to optimize?\n";
-		  cout << "Possible choices: ";
-		  for (auto fname : functions) {
-		    cout << fname.first << " ";
-		  }
-		  cout << endl;
-		  cin >> choice_fun;
-		  
-		  try {
-		    fun = functions.at(choice_fun);
-		  } catch (out_of_range) {
-		    cerr << "Bad choice" << endl;
-		    good_choice = false;
-		  }
-		} while(!good_choice);
-  
+      cout << "Which function to optimize?\n";
+      cout << "Possible choices: ";
+      for (auto fname : functions) {
+        cout << fname.first << " ";
+      }
+      cout << endl;
+      cin >> choice_fun;
+      
+      try {
+        fun = functions.at(choice_fun);
+      } catch (out_of_range) {
+        cerr << "Bad choice" << endl;
+        good_choice = false;
+      }
+    } while(!good_choice);
 
-		// Asking for the threshold below which a box is not split further
-		cout << "Precision? ";
-		cin >> precision; 
-	}
-  
-  // Broadcasting precision variable
+    // Asking for the threshold below which a box is not split further
+    cout << "Precision? ";
+    cin >> precision;
+
+    // Calcule pour taille de chaque "tranche" pour découper les intervalles
+    //suivant le nombre de machine
+    //double widthX = fun.x.width();
+    double n= (fun.x.width())/numProcs;
+    double nY=(fun.y.width())/numProcs;
+
+    //lower bound pour savoir ou commencer le découpage
+    double lowerBoundX=fun.x.left();
+    double lowerBoundY=fun.y.left();
+    
+    for ( int i=0;i<numProcs;++i ){
+      //On découpe l'intervale en X et en Y un nombre de fois
+      //égale au nombre de machines
+      interval inter (lowerBoundX,lowerBoundX+n);
+      interval interY(lowerBoundY,lowerBoundY+nY);
+      tabInterval[i]=inter;
+      tabIntervalY[i]=interY;
+      // lowerBound = upperBound pour passer au prochain intervale
+      lowerBoundX+=n;
+      lowerBoundY+=nY;
+    }
+  	
+  	bool enough_boxes;
+  	int size;
+  	
+  	subBoxes.push(fun.x);
+		subBoxes.push(fun.y);
+		do { 
+			// Je considère qu'on a plus de 1 procs donc on rentre au moins une fois dans la boucle
+			enough_boxes = true;
+			size = subBoxes.size();
+			// Divise chaque boite de la file en 4 nouvelles boites  	
+			for(int i = 0; i < size; i + 2) {
+				interval xl, xr, yl, yr;
+				split_box(subBoxes[0],subBoxes[1],xl,xr,yl,yr);
+				// Enlève la boite divisée
+				subBoxes.pop();
+				subBoxes.pop();
+				// Ajoutte les nouvelles boites
+				subBoxes.push(xl);
+				subBoxes.push(yl);
+				subBoxes.push(xl);
+				subBoxes.push(yr);
+				subBoxes.push(xr);
+				subBoxes.push(yl);
+				subBoxes.push(xr);
+				subBoxes.push(yr);
+			}
+			if(subBoxes.size() / 2 <= numprocs) {
+				// On arrête si on a suffisement de boites pour que chaque proc puisse en avoir une (au moins)
+				enough_boxes = false;
+			}
+		} while(!enough_boxes); 
+    SZ = subBoxes.size();
+  }
+
+  MPI_Bcast(&fun, sizeof(opt_fun_t), MPI_BYTE, 0, MPI_COMM_WORLD);
   MPI_Bcast(&precision, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&SZ, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  localSz = SZ % numProcs == 0 ? SZ / numProcs : (SZ / numProcs) *2;
   
-  // Dividing the initial box into enought sub-boxes to feed all procs
-  // J'ai l'impression que ça ressemble pas a grand chose.
-  // J'essaye de diviser la boite initiale de manière à ce qu'on en ai 
-  // le plus possible tout en ayant moins que le nombre max de procs.
+  interval myslice[localSZ];
   
-  // Je met la boite de base
-  subBoxes.push(fun.x);
-  subBoxes.push(fun.y);
-  do { 
-  	// Je considère qu'on a au moins 4 procs donc on rentre au moins une fois dans la boucle
-  	enough_boxes = true;
-  	size = subBoxes.size();
-  	// Divise chaque boite de la file en 4 nouvelles boites  	
-  	for(int i = 0; i < size; i + 2) {
-  		interval xl, xr, yl, yr;
-  		split_box(subBoxes[0],subBoxes[1],xl,xr,yl,yr);
-  		// Enlève les boite divisées
-  		subBoxes.pop();
-  		subBoxes.pop();
-  		// rajoutte les nouvelles boites
-  		subBoxes.push(xl);
-  		subBoxes.push(yl);
-  		subBoxes.push(xl);
-  		subBoxes.push(yr);
-  		subBoxes.push(xr);
-  		subBoxes.push(yl);
-  		subBoxes.push(xr);
-  		subBoxes.push(yr);
-  	}
-  	if(subBoxes.size() * 2 < numprocs) {
-  		// On arrête si la prochaine itération va générer plus de boites que le nombre de processus disponibles
-  		enough_boxes = false;
-  	}
-  } while(!enough_boxes); 
+  MPI_Scatter(&subBoxes,sizeof(interval)*localSZ,MPI_BYTE,&myslice,sizeof(interval)*localSZ,    MPI_BYTE,0,MPI_COMM_WORLD);
 
-  for (int i = 0; i < subBoxes.size(); ++i) {
-	  //MPI_Send();
+  //BCast des tableau d'intervalle 
+  //MPI_Bcast(&tabInterval,sizeof(interval)*numProcs,MPI_BYTE,0,MPI_COMM_WORLD);
+  //Chaque machine reçoit une du tableau d'interval en X -> on reçoit une ligne
+  /*
+  MPI_Scatter(&tabInterval,sizeof(interval),MPI_BYTE,&myslice,sizeof(interval),
+    MPI_BYTE,0,MPI_COMM_WORLD);
+   */
+  //BCast du tableau d'interval en 
+  //MPI_Bcast(&tabIntervalY,sizeof(interval)*numProcs,MPI_BYTE,0,MPI_COMM_WORLD);
+
+	/*
+  //Une thread en prallel pour chaque appelle de minimize
+  // Appelle de minimize par machine = nombre de machine
+  #pragma omp parallel 
+  {
+    double min_ub2 = numeric_limits<double>::infinity();
+     minimizer_list minimums;
+    #pragma omp for reduction(min : min_ub) 
+    for(int i=0;i<numProcs;++i){
+     
+     //Chaque machine s'occupe d'une "ligne" -> myslice[0]
+      // Chaque machine s'occupe un par un des cubes de la ligne -> tabIntervalY[i]
+      minimize(fun.f,myslice[0],tabIntervalY[i],precision,min_ub2,minimums);
+      //Si min trouver plus petit que le min actuel alors il devient le nouveau min 
+      if(min_ub>min_ub2) min_ub=min_ub2;
+      
+    }
+  }
+  */ 
+  minimizer_list minimums;
+  double min_ub2 = numeric_limits<double>::infinity();
+  for (int i = 0; i < localSZ; i = i+2) {
+  	minimize(fun.f,myslice[i],myslice[i+1],precision,min_ub2,minimums);
+  	if(min_ub>min_ub2) min_ub=min_ub2;
+  
   }
   
   
-  minimize(fun.f,fun.x,fun.y,precision,min_ub,minimums);
-  
+  // On fait un reduc pour trouver le plu petit min trouver par chaque machine
+  MPI_Reduce(&min_ub,&minUpAll,1,MPI_DOUBLE,MPI_MIN,0,MPI_COMM_WORLD);
   // Displaying all potential minimizers
-  copy(minimums.begin(),minimums.end(),
-       ostream_iterator<minimizer>(cout,"\n"));    
-  cout << "Number of minimizers: " << minimums.size() << endl;
-  cout << "Upper bound for minimum: " << min_ub << endl;*/
+  if (rank==0){
+    //copy(minimums.begin(),minimums.end(),
+    //ostream_iterator<minimizer>(cout,"\n"));    
+    //cout << "Number of minimizers: " << minimums.size() << endl;
+    cout << setprecision (6)<<"Upper bound for minimum: " << minUpAll << endl;
+
+  }
+  
+
+  MPI_Finalize();
 }
